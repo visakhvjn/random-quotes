966934edc67b4f3896575919b302aa63161842e6
Visakh Vijayan <visakh.vijayan@jalantechnologies.com>
2024-02-29 09:56:18 +0530

    lint fix

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -69,6 +69,7 @@ export const Main = () => {
             firstLoad = false;
             fetchNewQuote();
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
